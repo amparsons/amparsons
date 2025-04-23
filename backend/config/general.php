92d11c0610d8e74a256532421351cebfe9cd4996
Annemarie Parsons <annemarie@amparsons.co.uk>
2025-04-23 13:29:54 +0100

    [FIX] Reset general

--- backend/config/general.php
+++ backend/config/general.php
@@ -1,13 +1,29 @@
 <?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
 
+use craft\config\GeneralConfig;
 use craft\helpers\App;
 
-return [
-    'driver' => App::env('DB_DRIVER') ?: 'mysql',
-    'server' => App::env('DB_SERVER'),
-    'user' => App::env('DB_USER'),
-    'password' => App::env('DB_PASSWORD'),
-    'database' => App::env('DB_DATABASE'),
-    'port' => (int)(App::env('DB_PORT') ?: 3306),
-    'tablePrefix' => App::env('DB_TABLE_PREFIX') ?: '',
-];
+return GeneralConfig::create()
+    // Headless mode
+    ->headlessMode(true)
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
+    ->defaultWeekStartDay(1)
+    // Prevent generated URLs from including "index.php"
+    ->omitScriptNameInUrls()
+    // Preload Single entries as Twig variables
+    ->preloadSingles()
+    // Prevent user enumeration attacks
+    ->preventUserEnumeration()
+    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->aliases([
+        '@webroot' => dirname(__DIR__) . '/web',
+    ])
+;
