f0e7185358be377c631c74b0d05f8fb214b7700f
annemarie@amparsons.co.uk <annemarie@amparsons.co.uk>
2025-05-06 19:14:51 +0100

    [FIX] Remove server actions

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['freelance-frontend.ddev.site'], // adjust for your Craft CMS image domain
-  },
-  experimental: {
-    serverActions: true, // optional: only if you're using server actions
+    domains: ['amparsons.frb.io'], // adjust for your Craft CMS image domain
   },
 };
 
