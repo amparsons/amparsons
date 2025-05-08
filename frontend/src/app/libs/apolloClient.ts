e2d44cd7f034428ad65d19f0de2c6799445bcd40
annemarie@amparsons.co.uk <annemarie@amparsons.co.uk>
2025-05-08 19:59:45 +0100

    [FIX] Another log

--- frontend/src/app/libs/apolloClient.ts
+++ frontend/src/app/libs/apolloClient.ts
@@ -9,6 +9,9 @@ export function createApolloClient(): ApolloClient<NormalizedCacheObject> {
   const uri = process.env.NEXT_PUBLIC_CRAFT_CMS_GRAPHQL_ENDPOINT;
   const token = process.env.NEXT_PUBLIC_CRAFT_CMS_GRAPHQL_TOKEN;
 
+  console.log('GRAPHQL_ENDPOINT:', process.env.NEXT_PUBLIC_CRAFT_CMS_GRAPHQL_ENDPOINT);
+  console.log('GRAPHQL_TOKEN:', process.env.NEXT_PUBLIC_CRAFT_CMS_GRAPHQL_TOKEN);
+
   // Throw errors if required environment variables are not set
   if (!uri) {
     throw new Error('NEXT_PUBLIC_CRAFT_CMS_GRAPHQL_ENDPOINT is not defined');
