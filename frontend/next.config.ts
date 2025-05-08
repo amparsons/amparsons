2a27aa711a7b7e48297f4f213a79da0a53314c3d
annemarie@amparsons.co.uk <annemarie@amparsons.co.uk>
2025-05-08 20:18:01 +0100

    [FIX] Reset next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ['amparsons.frb.io'], // adjust for your Craft CMS image domain
   },
-  experimental: {
-    appDir: true, // If you're using the new app directory feature in Next.js 15
-  },
 };
 
 module.exports = nextConfig;
